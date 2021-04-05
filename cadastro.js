7a6f1952ce0efb37351c7e937372a30e7e6602f9
Adriano <lira23110806@gmail.com>
2021-04-05 18:34:59 -0300

    insert atual

--- cadastro.js
+++ cadastro.js
@@ -1,5 +1,7 @@
 function confirmar() {
     if(document.getElementById("dados").value.length== " "){
         alert("Por favor, confira seu cadastro e insira as informações necessárias!")
+}else {
+    alert("Cadastro efetuado com sucesso!")
 }
 }
