516e23b9c3afa3996e266c0c12433275c0acf274
Adriano <lira23110806@gmail.com>
2021-04-09 17:47:10 -0300

    oit alt

---
+++ principal.js
@@ -0,0 +1,7 @@
+function usuario(){
+    if(document.getElementById("e-mail").value.length== " "){
+        alert("Insira os dados de usuário!")
+    }if(document.getElementById("password").value.length== " "){
+        alert("Por favor insira a senha!")
+    }
+}
